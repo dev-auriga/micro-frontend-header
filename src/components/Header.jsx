b03ceda20bf713bdc4df7101d0807a967daeca68
Dev Kumar <dev.kumar@aurigait..com>
2025-08-03 12:27:19 +0530

    Basic Webpack setup for Micro-frontend & Module Federation Implementation

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header>
+        <h1>Micro-frontend & Module Federation Header</h1>
+    </header>
+  )
+}
+
+export default Header
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "development",
+  devServer: {
+    port: 3010,
+    historyApiFallback: true,
+  },
+  output: {
+    publicPath: "auto",
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    clean: true,
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "header",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./Header": "./src/components/Header",
+      },
+      shared: {
+        react: { singleton: true, requiredVersion: false },
+        "react-dom": { singleton: true, requiredVersion: false },
+      },
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
